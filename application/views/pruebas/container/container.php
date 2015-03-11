07d7dacae652f77140c6f3d479a9a0c65aca9e7a
Julio Trujillo <julio.trujillo.alvarado@outlook.com>
2015-03-11 16:43:48 +0000

    repositorio para hacer pruebas de cualquier tipo

---
+++ application/views/pruebas/container/container.php
@@ -0,0 +1,9 @@
+<body>
+
+<div id="container">
+	<h1>Página de pruebas</h1>
+
+	<div id="body">
+		<p id="target">Aquí irán las pruebas</p>
+	</div>
+</div>
---
+++ application/views/pruebas/footer/footer.php
@@ -0,0 +1,14 @@
+<!-- INICIO DE PLUGINS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!-- FIN DE PLUGINS -->
+
+	<!-- INICIO DE SCRIPTS -->
+		<script>
+			jQuery(document).ready(function() {
+				
+			});
+		</script>
+	<!-- FIN DE SCRIPTS -->
+	</body>
+	<!-- END BODY -->
+</html>
